2c00f7ecf5ce3450ed26357f8f4dbcaf44f34203
MiYu-C <ninningao@qq.com>
2020-03-26 23:40:52 +0800

    修复问题

--- src/web/routes/dashboard/index.vue
+++ src/web/routes/dashboard/index.vue
@@ -4,7 +4,8 @@
       <div slot="header" class="text">
         <span>欢迎：管理员</span>
       </div>
-      <panel-group @handleSetLineChartData="handleSetLineChartData" />
+      <!-- <panel-group @handleSetLineChartData="handleSetLineChartData" /> -->
+      <panel-group />
       <!-- <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
       <line-chart :chart-data="lineChartData" />
     </el-row> -->
@@ -52,23 +53,23 @@ import BarChart from './components/BarChart'
 // import TransactionTable from './components/TransactionTable'
 // import TodoList from './components/TodoList'
 // import BoxCard from './components/BoxCard'
-const lineChartData = {
-  // newVisitis: {
-  //   expectedData: [100, 120, 161, 134, 105, 160, 165]
-  // },
-  // messages: {
-  //   expectedData: [200, 192, 120, 144, 160, 130, 140],
-  //   actualData: [180, 160, 151, 106, 145, 150, 130]
-  // },
-  // purchases: {
-  //   expectedData: [80, 100, 121, 104, 105, 90, 100],
-  //   actualData: [120, 90, 100, 138, 142, 130, 130]
-  // },
-  // shoppings: {
-  //   expectedData: [130, 140, 141, 142, 145, 150, 160],
-  //   actualData: [120, 82, 91, 154, 162, 140, 130]
-  // }
-}
+// const lineChartData = {
+//   newVisitis: {
+//     expectedData: [100, 120, 161, 134, 105, 160, 165]
+//   },
+//   messages: {
+//     expectedData: [200, 192, 120, 144, 160, 130, 140],
+//     actualData: [180, 160, 151, 106, 145, 150, 130]
+//   },
+//   purchases: {
+//     expectedData: [80, 100, 121, 104, 105, 90, 100],
+//     actualData: [120, 90, 100, 138, 142, 130, 130]
+//   },
+//   shoppings: {
+//     expectedData: [130, 140, 141, 142, 145, 150, 160],
+//     actualData: [120, 82, 91, 154, 162, 140, 130]
+//   }
+// }
 export default {
   name: 'DashboardAdmin',
   components: {
@@ -84,13 +85,13 @@ export default {
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
+      // lineChartData: lineChartData.newVisitis
     }
   },
   methods: {
-    handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
-    }
+    // handleSetLineChartData(type) {
+    //   this.lineChartData = lineChartData[type]
+    // }
   }
 }
 </script>
