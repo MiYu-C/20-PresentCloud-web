027d894655357921a09d8b9dcaee314084529e5e
MiYu-C <ninningao@qq.com>
2020-06-17 19:52:12 +0800

    更新菜单管理界面

--- src/web/api/dictionary.js
+++ src/web/api/dictionary.js
@@ -1,9 +1,50 @@
 import request from '@/web/utils/request'
 
-export function getData(currentPage, pagesize) {
+export function getList(currentPage, pagesize, name) {
   return request({
     url: '/present-cloud/dictionary/data',
     method: 'get',
-    params: { currentPage, pagesize }
+    params: { currentPage, pagesize, name }
   })
 }
+
+export function getLabel(id) {
+  return request({
+    url: '/present-cloud/dictionary/label',
+    method: 'get',
+    params: { id }
+  })
+}
+
+export function updateList(form, dictData) {
+  return request({
+    url: '/present-cloud/dictionary/update',
+    method: 'put',
+    params: { form, dictData }
+  })
+}
+
+export function deleteItem(form) {
+  return request({
+    url: '/present-cloud/dictionary/delete',
+    method: 'delete',
+    params: { form }
+  })
+}
+
+export function addItem(form, dictData) {
+  return request({
+    url: '/present-cloud/dictionary/add',
+    method: 'post',
+    params: { form, dictData }
+  })
+}
+
+export function isExist(id, name) {
+  return request({
+    url: '/present-cloud/dictionary/exist',
+    method: 'get',
+    params: { id, name }
+  })
+}
+
