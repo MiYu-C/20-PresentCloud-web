5622f1cde02c43ce33e76ee81d278f1eefc190b1
MiYu-C <ninningao@qq.com>
2020-06-17 19:53:02 +0800

    Revert "更新菜单管理界面"
    This reverts commit 027d894655357921a09d8b9dcaee314084529e5e.

--- src/web/api/dictionary.js
+++ src/web/api/dictionary.js
@@ -1,50 +1,9 @@
 import request from '@/web/utils/request'
 
-export function getList(currentPage, pagesize, name) {
+export function getData(currentPage, pagesize) {
   return request({
     url: '/present-cloud/dictionary/data',
     method: 'get',
-    params: { currentPage, pagesize, name }
+    params: { currentPage, pagesize }
   })
 }
-
-export function getLabel(id) {
-  return request({
-    url: '/present-cloud/dictionary/label',
-    method: 'get',
-    params: { id }
-  })
-}
-
-export function updateList(form, dictData) {
-  return request({
-    url: '/present-cloud/dictionary/update',
-    method: 'put',
-    params: { form, dictData }
-  })
-}
-
-export function deleteItem(form) {
-  return request({
-    url: '/present-cloud/dictionary/delete',
-    method: 'delete',
-    params: { form }
-  })
-}
-
-export function addItem(form, dictData) {
-  return request({
-    url: '/present-cloud/dictionary/add',
-    method: 'post',
-    params: { form, dictData }
-  })
-}
-
-export function isExist(id, name) {
-  return request({
-    url: '/present-cloud/dictionary/exist',
-    method: 'get',
-    params: { id, name }
-  })
-}
-
