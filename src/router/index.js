8c8f64eff0ecafbfa190fe558185aa928e8f211e
MiYu-C <ninningao@qq.com>
2020-07-05 18:12:24 +0800

    调整首页

--- src/router/index.js
+++ src/router/index.js
@@ -119,13 +119,40 @@ export const constantRoutes = [
             path: 'subject',
             name: '静态课程管理',
             component: () => import('@/web/routes/school/subject/index'),
-            meta: { title: '课程管理改', icon: 'behavior' }
+            meta: { title: '课程管理', icon: 'behavior' }
+          }
+        ]
+      },
+      {
+        path: 'error',
+        redirect: '/example/error/404',
+        name: 'error',
+        component: () => import('@/web/routes/school/index'),
+        meta: { title: '异常界面', icon: 'education' },
+        children: [
+          {
+            path: '404',
+            name: '404',
+            component: () => import('@/web/routes/404'),
+            meta: { title: '404', icon: '404' }
           },
           {
-            path: 'class',
-            name: '静态课程管理',
-            component: () => import('@/web/routes/school/class/index'),
-            meta: { title: '课程管理', icon: 'behavior' }
+            path: '403',
+            name: '403',
+            component: () => import('@/web/routes/403'),
+            meta: { title: '403', icon: '403' }
+          },
+          {
+            path: '500',
+            name: '500',
+            component: () => import('@/web/routes/500'),
+            meta: { title: '500', icon: '500' }
+          },
+          {
+            path: 'def',
+            name: 'def',
+            component: () => import('@/web/routes/def'),
+            meta: { title: '自定义异常', icon: 'behavior' }
           }
         ]
       },
@@ -141,12 +168,6 @@ export const constantRoutes = [
         component: () => import('@/web/routes/list/index'),
         meta: { title: '列表', icon: 'list' }
       }
-      // {
-      //   path: 'form',
-      //   name: '编辑',
-      //   component: () => import('@/web/routes/form/index'),
-      //   meta: { title: '编辑', icon: 'form' }
-      // }
     ]
   },
   {
@@ -155,18 +176,6 @@ export const constantRoutes = [
     hidden: true,
     redirect: 'noredirect',
     children: [
-      // {
-      //   path: 'manager',
-      //   name: '用户管理',
-      //   component: () => import('@/web/routes/user/manager/index'),
-      //   meta: { title: '用户管理', icon: 'form' }
-      // },
-      // {
-      //   path: 'teacher',
-      //   name: '用户管理改',
-      //   component: () => import('@/web/routes/user/teacher/index'),
-      //   meta: { title: '用户管理改', icon: 'form' }
-      // },
       {
         path: 'center',
         component: () => import('@/web/routes/user/center/index'),
@@ -176,75 +185,6 @@ export const constantRoutes = [
       }
     ]
   }
-  // {
-  //   path: '/role',
-  //   component: Layout,
-  //   redirect: '/role/roleList',
-  //   name: '角色管理',
-  //   meta: { title: '角色管理', icon: 'role' },
-  //   children: [
-  //     {
-  //       path: 'roleList',
-  //       name: '角色管理',
-  //       component: () => import('@/web/routes/role/index'),
-  //       meta: { title: '角色管理', icon: 'role' }
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/system',
-  //   component: Layout,
-  //   redirect: '/system/dictionary',
-  //   name: '系统管理',
-  //   meta: { title: '系统管理', icon: 'monitor' },
-  //   children: [
-  //     {
-  //       path: 'dictionary',
-  //       name: '数据字典',
-  //       component: () => import('@/web/routes/system/dictionary/index'),
-  //       meta: { title: '数据字典', icon: 'documentation' }
-  //     },
-  //     {
-  //       path: 'menu',
-  //       name: '菜单管理',
-  //       component: () => import('@/web/routes/system/menu/index'),
-  //       meta: { title: '菜单管理', icon: 'table' }
-  //     }
-  //   ]
-  // }
-  // {
-  //   path: '/school',
-  //   component: Layout,
-  //   redirect: '/school/schoolList',
-  //   name: '学校管理',
-  //   meta: { title: '学校管理', icon: 'education' },
-  //   children: [
-  //     {
-  //       path: 'schoolList',
-  //       name: '院校管理',
-  //       component: () => import('@/web/routes/school/school/index'),
-  //       meta: { title: '院校管理', icon: 'school1' }
-  //     },
-  //     {
-  //       path: 'study',
-  //       name: '学习行为管理',
-  //       component: () => import('@/web/routes/school/study/index'),
-  //       meta: { title: '学习行为管理', icon: 'behavior' }
-  //     },
-  //     {
-  //       path: 'subject',
-  //       name: '课程管理',
-  //       component: () => import('@/web/routes/school/subject/index'),
-  //       meta: { title: '课程管理', icon: 'behavior' }
-  //     },
-  //     {
-  //       path: 'class',
-  //       name: '班课管理',
-  //       component: () => import('@/web/routes/school/class/index'),
-  //       meta: { title: '班课管理', icon: 'behavior' }
-  //     }
-  //   ]
-  // }
 ]
 
 const createRouter = () => new Router({
